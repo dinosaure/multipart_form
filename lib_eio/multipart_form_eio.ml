2b55b389411f03067d1f04a2c9a4642b1d7f7b27
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2023-04-09 11:20:22 +0200

    Add eio variant

---
+++ lib_eio/multipart_form_eio.ml
@@ -0,0 +1,80 @@
+open Multipart_form
+
+let stream ~sw ?(bounds = 10) ~identify stream content_type =
+  let output = Eio.Stream.create max_int in
+  let q = Queue.create () in
+  let fresh_id =
+    let r = ref 0 in
+    fun () ->
+      incr r ;
+      !r in
+  let tbl = Hashtbl.create 0x10 in
+  let emitters header =
+    let id = fresh_id () in
+    Queue.push (`Id (header, id)) q ;
+    ((fun data -> Queue.push (`Data (id, data)) q), id) in
+  let parse = Multipart_form.parse ~emitters content_type in
+  let promise, resolve = Eio.Promise.create () in
+  let rec go () =
+    match Queue.pop q with
+    | `Id (header, id) ->
+        let client_id = identify header in
+        let stream = Eio.Stream.create bounds in
+        Hashtbl.add tbl id (client_id, stream) ;
+        Eio.Stream.add output (client_id, header, stream);
+        go ()
+    | `Data (id, Some data) ->
+        let _, stream = Hashtbl.find tbl id in
+        Eio.Stream.add stream data;
+        go ()
+    | `Data (_, None) ->
+        (* let _, stream = Hashtbl.find tbl id in *)
+        (* stream#close; *)
+        go ()
+    | exception Queue.Empty -> (
+        (* otherwise, continue parsing (thus adding elements to the queue) *)
+        let data = match Eio.Stream.take stream with Some s -> `String s | None -> `Eof in
+        match parse data with
+        | `Continue -> go ()
+        | `Done t ->
+            let client_id_of_id id =
+              let client_id, _ = Hashtbl.find tbl id in
+              client_id in
+            Eio.Promise.resolve_ok resolve (map client_id_of_id t)
+        | `Fail _ ->
+            Eio.Promise.resolve_error resolve (`Msg "Invalid multipart/form"))
+  in
+  Eio.Fiber.fork ~sw go;
+  (promise, output)
+
+(* only used internally to implement of_stream_to_{tree,list} *)
+let of_stream_to_tbl s content_type =
+  let identify =
+    let id = ref (-1) in
+    fun _header ->
+      incr id ;
+      !id in
+  Eio.Switch.run @@ fun sw ->
+  let t, parts = stream ~sw ~identify s content_type in
+  let parts_tbl = Hashtbl.create 0x10 in
+  let consume_part (id, _, part_stream) =
+    Eio.Stream.take part_stream
+    |> Hashtbl.add parts_tbl id
+  in
+  Eio.Fiber.fork ~sw (fun () ->
+      while not @@ Eio.Stream.is_empty parts do
+        Eio.Stream.take parts |> consume_part
+      done);
+  Eio.Promise.await t
+  |> Result.map (fun tree -> (tree, parts_tbl))
+
+let of_stream_to_tree s content_type =
+  of_stream_to_tbl s content_type
+  |> Result.map (fun (tree, parts_tbl) -> map (Hashtbl.find parts_tbl) tree)
+
+let of_stream_to_list s content_type =
+  of_stream_to_tbl s content_type
+  |> Result.map
+    (fun (tree, parts_tbl) ->
+       let assoc = Hashtbl.fold (fun k b a -> (k, b) :: a) parts_tbl [] in
+       (tree, assoc))
---
+++ lib_eio/multipart_form_eio.mli
@@ -0,0 +1,63 @@
+open Multipart_form
+
+(** {3 Streaming API.} *)
+
+val stream :
+  sw:Eio.Switch.t ->
+  ?bounds:int ->
+  identify:(Header.t -> 'id) ->
+  string option Eio.Stream.t ->
+  Content_type.t ->
+  ('id t, [> `Msg of string ]) result Eio.Promise.t
+  * ('id * Header.t * string Eio.Stream.t) Eio.Stream.t
+(** [stream ~identify src content_type] returns:
+    - a promise [th] about the parser
+    - a stream [stream] of parts
+
+    They can be manipulated with [Lwt.both], and, by this way,
+    ensure a real stream between the parser [th] and the process
+    which saves parts.
+
+    Assume that you have a function to save a [Lwt_stream.t] into
+    a [filename]:
+    {[
+      val save_part : filename:string -> Header.t -> string Lwt_stream.t ->
+        unit Lwt.t
+    ]}
+
+    You can use it with [stream] like:
+    {[
+      let identify _ : string = random_unique_filename () in
+      let `Parse th, stream = stream ~identify src content_type in
+      let rec saves () = Lwt_stream.get stream >>= function
+        | None -> Lwt.return_unit
+        | Some (filename, hdr, contents) ->
+          save_part ~filename hdr contents >>= fun () ->
+          saves () in
+      Lwt.both th (saves ()) >>= fun (res, ()) -> Lwt.return res
+    ]}
+
+    By this way, as long as we parse [src], at the same time, we save parts
+    into filenames. Finally, we return the [multipart/form] structure with
+    a mapping between temporary files and parts. *)
+
+(** {3 Non-streaming API.}
+
+    These functions will store the entire multipart contents in memory,
+    and therefore should not be used when handling possibly large data. *)
+
+val of_stream_to_list :
+  string option Eio.Stream.t ->
+  Content_type.t ->
+  (int t * (int * string) list, [> `Msg of string ]) result
+(** Similar to [Multipart_form.of_stream_to_list], but consumes a
+   [Lwt_stream.t]. *)
+
+val of_stream_to_tree :
+  string option Eio.Stream.t ->
+  Content_type.t ->
+  (string t, [> `Msg of string ]) result
+(** [of_stream_to_tree stream content_type] returns, if it succeeds, a value
+   {!Multipart_form.t} representing the multipart document, where the contents of the parts are
+   stored as strings. It is equivalent to [of_stream_to_list] where references
+   have been replaced with their associated contents. *)
